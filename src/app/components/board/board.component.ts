feae9c6dda076613154703ff9a977d686dc97868
snimmagadda1 <saiguy@me.com>
2024-02-15 16:45:20 -0500

    Scaffolding

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-board',
@@ -10,15 +16,24 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
       <h1>Board Component</h1>
       <p>This is an inline template</p>
     </div>
-    <canvas #board></canvas>
+    <canvas
+      #board
+      [width]="COLUMNS * (CELL_SIZE + GAP_SIZE)"
+      [height]="ROWS * (CELL_SIZE + GAP_SIZE)"></canvas>
   `,
 })
-export class BoardComponent {
+export class BoardComponent implements AfterViewInit {
   public readonly COLUMNS = 50;
-  public readonly ROWs = 50;
-
+  public readonly ROWS = 50;
   public readonly CELL_SIZE = 10;
   public readonly GAP_SIZE = 1;
 
-  @ViewChild('board', { static: true }) board!: ElementRef;
+  @ViewChild('board') board!: ElementRef<HTMLCanvasElement>;
+  context!: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    this.context = this.board.nativeElement.getContext('2d')!;
+    this.context.fillStyle = 'red';
+    this.context.fillRect(20, 20, 100, 100);
+  }
 }
