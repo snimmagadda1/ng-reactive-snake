b28f457fb32bf55758f59e610be60d5f3bde2211
snimmagadda1 <saiguy@me.com>
2024-02-15 15:33:21 -0500

    Install formatters

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-board',
@@ -10,8 +10,15 @@ import { Component } from '@angular/core';
       <h1>Board Component</h1>
       <p>This is an inline template</p>
     </div>
-  `
+    <canvas #board></canvas>
+  `,
 })
 export class BoardComponent {
+  public readonly COLUMNS = 50;
+  public readonly ROWs = 50;
 
+  public readonly CELL_SIZE = 10;
+  public readonly GAP_SIZE = 1;
+
+  @ViewChild('board', { static: true }) board!: ElementRef;
 }
